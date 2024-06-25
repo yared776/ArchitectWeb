d117a0e2863509e94b734ffba33f212dd3189bee
Yared A. Beriso <73754399+yared776@users.noreply.github.com>
2024-06-25 07:18:21 +0300

    inst

---
+++ architectsweb/app/page.tsx
@@ -0,0 +1,10 @@
+import LandingPage from '../LandingPage';
+import FAQ from '../components/FAQ';
+
+export default function Home() {
+  return (
+    <main>
+      <LandingPage/>
+    </main>
+  );
+}
